ca786db76195605b03ce729f0608ce0a134bdcc5
Nick Ibarra <e-nibarra@neoris.com>
2019-10-25 20:46:50 -0500

    Initial Commit

---
+++ Albums-Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums-Challenge
+//
+//  Created by Nick Ibarra on 25/10/19.
+//  Copyright © 2019 Nick iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
